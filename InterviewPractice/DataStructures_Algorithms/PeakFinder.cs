99303424be96a3d382578297b74c1bf842e685a1
dilansudharaka <dilanh@outlook.com>
2017-10-05 21:59:32 -0700

    reimplement 1D Peak

--- InterviewPractice/DataStructures_Algorithms/PeakFinder.cs
+++ InterviewPractice/DataStructures_Algorithms/PeakFinder.cs
@@ -12,19 +12,19 @@ namespace DataStructures_Algorithms
         static void Main(string[] args)
         {
             int[] arr = { 10 };
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, 0));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
 
             arr = new int[] { 1, 2, 3, 4, 5 };
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, arr.Length - 1));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
 
             arr = new int[] { 1, 200, 40, 50, 65, 70, 46, 27 };
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, arr.Length));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
 
             arr = new int[] { 1, 3, 20, 4, 1, 0 };
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, arr.Length));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
 
             arr = new int[] { 1, 1, 1, 1 };
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, arr.Length));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
 
             arr = new int[100000000];
             Random r = new Random();
@@ -34,71 +34,56 @@ namespace DataStructures_Algorithms
             }
 
             Stopwatch w = Stopwatch.StartNew();
-            Console.WriteLine(PeakFinder.FindPeak(arr, 0, arr.Length));
             Console.WriteLine($"Total time taken: { w.ElapsedMilliseconds }");
             w.Stop();
 
             w = Stopwatch.StartNew();
-            Console.WriteLine(PeakFinder.FindPeakLinear(arr));
+            Console.WriteLine(PeakFinder.Find1DPeak(arr));
+            Console.WriteLine($"Total time taken: { w.ElapsedMilliseconds }");
+            w.Stop();
+
+            w = Stopwatch.StartNew();
             Console.WriteLine($"Total time taken: { w.ElapsedMilliseconds }");
             w.Stop();
 
         }
 
-        private static int FindPeakLinear(int[] array)
+        private static int Find1DPeak(int[] array)
         {
-            for(int i=0; i<array.Length; i++)
+            if(array == null || array.Length == 0)
             {
-                if (i == 0)
-                {
-                    if (array[i] >= array[i + 1])
-                    {
-                        return array[i];
-                    }
-                }
-                else if (i == array.Length - 1)
-                {
-                    if(array[i] >= array[i-1])
-                    {
-                        return array[i];
-                    }
-                }
-                else
-                {
-                    if(array[i] >= array[i-1] && array[i] >= array[i+1])
-                    {
-                        return array[i];
-                    }
-                }                
+                throw new ArgumentException();
             }
 
-            return -1;
+            if(array.Length == 1)
+            {
+                return array[0];
+            }
+
+            return Find1DPeakRecursive(array, 0, array.Length - 1);
         }
 
-
-        private static int FindPeak(int[] array, int startIndex, int endIndex)
+        private static int Find1DPeakRecursive(int[] input, int startIndex, int endIndex)
         {
-            // base case.
-            if(endIndex == startIndex)
+            int itemCount = endIndex - startIndex + 1;
+            if(itemCount == 1)
             {
-                return array[startIndex];
+                return input[endIndex];
             }
 
             int midIndex = (startIndex + endIndex) / 2;
-            if((midIndex == 0 || array[midIndex-1] <= array[midIndex]) 
-                && (midIndex == array.Length -1 || array[midIndex] >= array[midIndex + 1]))
+            if(input[midIndex] < input[midIndex + 1])
             {
-                return array[midIndex];
+                return Find1DPeakRecursive(input, midIndex + 1, endIndex);
             }
 
-            if (midIndex > 0 && array[midIndex - 1] > array[midIndex])
+            if(input[midIndex] < input[midIndex - 1])
             {
-                return FindPeak(array, startIndex, midIndex - 1);
+                return Find1DPeakRecursive(input, startIndex, midIndex - 1);
             }
-            else
-            {
-                return FindPeak(array, midIndex + 1, endIndex);
-            }     
+
+            return input[midIndex];
         }
+        
     }
 }
